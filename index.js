2bc8bc4b740de63d9f13ab57a055db5c558c7b51
tahsinkarim <tahsinnkarim@gmail.com>
2022-11-08 19:25:46 +0600

    Added get review by id and user

--- index.js
+++ index.js
@@ -18,6 +18,7 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 async function run(){
     try{
         const photoCollection = client.db('photographer').collection('photoService')
+        const reviewCollection = client.db('photographer').collection('reviews')
 
         //Get all services
         app.get('/services', async (req,res)=> {
@@ -31,6 +32,31 @@ async function run(){
         app.get('/service', async (req,res)=> {
             const query = {}
             const cursor = photoCollection.find(query).limit(3)
+            const result = await cursor.toArray()
+            res.send(result)
+        })
+        //Create New review
+        app.post('/reviews', async (req, res)=>{
+            const review = req.body
+            console.log(review)
+            const result = await reviewCollection.insertOne(review)
+            res.send(result)
+        })
+
+        //Get review by service
+        app.get('/reviews/:id', async (req,res)=> {
+            const id = req.params.id
+            const query = { service: id}
+            const cursor = reviewCollection.find(query)
+            const result = await cursor.toArray()
+            res.send(result)
+        })
+
+        //Get review by user query
+        app.get('/reviews', async (req,res)=> {
+            const user = req.query.user
+            const query = { user : user}
+            const cursor = reviewCollection.find(query)
             const result = await cursor.toArray()
             res.send(result)
         })
